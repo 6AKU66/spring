fb0d669150299396fb12aeeb3634221e266b411b
AKU <89547444+6AKU66@users.noreply.github.com>
2024-03-21 15:31:19 +0300

    Revert last commit

--- rts/System/Platform/Win/CriticalSection.cpp
+++ rts/System/Platform/Win/CriticalSection.cpp
@@ -3,33 +3,40 @@
 #include "CriticalSection.h"
 #include <algorithm>
 
+#include <tracy/Tracy.hpp>
+
 
 CriticalSection::CriticalSection() noexcept
 {
+	//ZoneScoped;
 	InitializeCriticalSection(&mtx);
 }
 
 
 CriticalSection::~CriticalSection()
 {
+	//ZoneScoped;
 	DeleteCriticalSection(&mtx);
 }
 
 
 void CriticalSection::lock()
 {
+	//ZoneScoped;
 	EnterCriticalSection(&mtx);
 }
 
 
 bool CriticalSection::try_lock() noexcept
 {
+	//ZoneScoped;
 	return TryEnterCriticalSection(&mtx);
 }
 
 
 void CriticalSection::unlock()
 {
+	//ZoneScoped;
 	LeaveCriticalSection(&mtx);
 }
 
@@ -38,21 +45,25 @@ void CriticalSection::unlock()
 win_signal::win_signal() noexcept
 : sleepers(false)
 {
+	//ZoneScoped;
 	event = CreateEvent(NULL, true, false, NULL);
 }
 
 win_signal::~win_signal()
 {
+	//ZoneScoped;
 	CloseHandle(event);
 }
 
 void win_signal::wait()
 {
+	//ZoneScoped;
 	wait_for(spring_notime);
 }
 
 void win_signal::wait_for(spring_time t)
 {
+	//ZoneScoped;
 	++sleepers;
 
 	const DWORD timeout_milliseconds = t.toMilliSecsi();
@@ -66,6 +77,7 @@ void win_signal::wait_for(spring_time t)
 
 void win_signal::notify_all(const int min_sleepers)
 {
+	//ZoneScoped;
 	if (sleepers.load() < std::max(1, min_sleepers))
 		return;
 
